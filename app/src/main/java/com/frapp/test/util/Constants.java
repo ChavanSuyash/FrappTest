450abcce09758be26d5e6b60537c7317183387d7
Suyash <suyash.chavan9@gmail.com>
2018-11-17 23:04:46 +0530

    Update : Remove favourite items logic added

--- app/src/main/java/com/frapp/test/util/Constants.java
+++ app/src/main/java/com/frapp/test/util/Constants.java
@@ -2,9 +2,14 @@ package com.frapp.test.util;
 
 public class Constants {
 
+    public final static String internships = "Internships";
+    public final static String missions = "Missions";
+
     public final static String nonFeaturedInternships = "NonFeaturedInternships";
     public final static String nonFeaturedMissions = "NonFeaturedMissions";
     public final static String featuredInternships = "FeaturedInternships";
     public final static String featuredMissions = "FeaturedMissions";
+
+    public final static String dataModelListTag = "DataModelList";
 
 }
--- app/src/main/java/com/frapp/test/util/Utils.java
+++ app/src/main/java/com/frapp/test/util/Utils.java
@@ -1,5 +1,7 @@
 package com.frapp.test.util;
 
+import android.util.Log;
+
 import com.frapp.test.data.DataModel;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -10,6 +12,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Deque;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class Utils {
@@ -42,18 +45,36 @@ public class Utils {
         return listOne;
     }
 
-    public static List<DataModel> getInterleavedFinalLists(List<DataModel> featuredList, List<DataModel> nonFeaturedList) {
+    public static List<DataModel> getInterleavedFinalLists(List<DataModel> featuredList, List<DataModel> nonFeaturedList, Map<String, List<DataModel>> toRemoveItemListMap) {
 
-        int position = 0, previousPosition;
+        int position = 0, previousPosition, result;
+
+
+        //Remove Favourite items
+        for (Map.Entry<String, List<DataModel>> entry : toRemoveItemListMap.entrySet()) {
+            List<DataModel> toRemoveItem = entry.getValue();
+            for(int i = 0 ; i < toRemoveItem.size() ; i++){
+
+                result = getItemPositionInList(featuredList, toRemoveItem.get(i).getTitle(), toRemoveItem.get(i).getDescription());
+                if(result != -1){
+                    featuredList.remove(position);
+                }
+
+                result = getItemPositionInList(nonFeaturedList, toRemoveItem.get(i).getTitle(), toRemoveItem.get(i).getDescription());
+                if(result != -1){
+                    nonFeaturedList.remove(position);
+                }
+            }
+        }
 
         //lists are setup as queues
         Deque<DataModel> featuredListQueue = new ArrayDeque<>(featuredList);
         Deque<DataModel> nonFeatureListQueue = new ArrayDeque<>(nonFeaturedList);
 
         List<DataModel> repeatedFeaturedList = new ArrayList<>();
-        List<DataModel> result = new ArrayList<>(featuredListQueue.size() + nonFeatureListQueue.size());
+        List<DataModel> resultList = new ArrayList<>(featuredListQueue.size() + nonFeatureListQueue.size());
 
-        result.add(featuredListQueue.poll());
+        resultList.add(featuredListQueue.poll());
         position++;
         previousPosition = position;
 
@@ -62,30 +83,40 @@ public class Utils {
             // if the position of featuredList is greater then or equal to current position of element to be added
             // then add the element from featuredList queue
             if (Objects.requireNonNull(featuredListQueue.peek()).getPosition() == position) {
-                result.add(featuredListQueue.poll());
+                resultList.add(featuredListQueue.poll());
                 previousPosition = position;
                 position++;
             } else {
                 // if item in featuredList is repeated add it to queue later
-                if(previousPosition == Objects.requireNonNull(featuredListQueue.peek()).getPosition()){
+                if (previousPosition == Objects.requireNonNull(featuredListQueue.peek()).getPosition()) {
                     repeatedFeaturedList.add(featuredListQueue.poll());
-                }else{
-                    result.add(nonFeatureListQueue.poll());
+                } else {
+                    resultList.add(nonFeatureListQueue.poll());
                     position++;
                 }
             }
         }
         // since the while loop terminates when one of the queues is empty
         // we should add the left-over elements
-        result.addAll(repeatedFeaturedList);
-        result.addAll(featuredListQueue);
-        result.addAll(nonFeatureListQueue);
+        resultList.addAll(repeatedFeaturedList);
+        resultList.addAll(featuredListQueue);
+        resultList.addAll(nonFeatureListQueue);
 
-        return result;
+        for(DataModel dataModel : resultList)
+            Log.e("FinalList",dataModel.getTitle() + "**" + dataModel.getPosition());
+        return resultList;
+    }
+
+    private static int getItemPositionInList(List<DataModel> dataModelList, String title, String description){
+        for(int i = 0 ; i < dataModelList.size() ; i++){
+            if(dataModelList.get(i).getTitle().equals(title) && dataModelList.get(i).getDescription().equals(description))
+                return i;
+        }
+        return -1;
     }
 
     public static Gson getGsonParser() {
-        if(null == gson) {
+        if (null == gson) {
             GsonBuilder builder = new GsonBuilder();
             gson = builder.create();
         }
